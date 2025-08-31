8570064eae4980a8ebd77fcc13a5b8dc3c7e4a7f
Arpit Singh <arpitsingh2005mbd@gmail.com>
2025-08-31 03:02:40 -0700

    second commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,37 @@
-import { Text, View } from "react-native";
+import { BatteryVerticalFullIcon, BluetoothIcon, CellSignalFullIcon } from "phosphor-react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function FirstPage() {
     return(
-        <View>
-            <Text>^Rf6tfugj7yji</Text>
+       <View>
+        <View style={styles.header}>
+            <Text>14:39</Text>
+            <View style={styles.headerLeft}>
+                <BluetoothIcon size={16} />
+                <CellSignalFullIcon size={16} />
+                <BatteryVerticalFullIcon size={16} />
+            </View>
         </View>
+        <View></View>
+        <View></View>
+        <View></View>
+        <View></View>
+        <View></View>
+       </View>
+           
     )
 }
+
+const styles=StyleSheet.create({
+    header: {
+        marginTop: 40,
+        marginHorizontal: 20,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    headerLeft: {
+        flexDirection: "row",
+        gap: 5
+    }
+})
